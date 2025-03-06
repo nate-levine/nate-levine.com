8b7ee70dc1ae35e56ea09293512a7d901aab3223
nate-levine <n8levine11@gmail.com>
2025-03-06 16:30:45 -0500

    Updated main pages hero image

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
 
     return (
         <div>
-            <header className="bg-[url('../images/input.png')] bg-[position:0%_56%] bg-cover py-8 border-solid border-b-2 border-black">
+            <header className="bg-[url('../images/output.png')] bg-[position:0%_56%] bg-cover py-8 border-solid border-b-2 border-black">
                 <NavigationBar/>
             </header>
             <main className="bg-primary">
